601f43d5a884b4137609e2740896fa918710acf2
matt <paynmatt@gmail.com>
2008-08-03 18:50:35 -0400

    Fixing requires

--- application.rb
+++ application.rb
@@ -8,7 +8,9 @@ require 'src/config'
 local_dir = File.expand_path(File.dirname(__FILE__))
 
 MattPayne::Config.vendored_items.each do |vendor_dir, require_file|
-	require File.join(local_dir, vendor_dir.to_s, require_file.to_s)	
+	path = File.expand_path(File.join(local_dir, vendor_dir.to_s))
+	$:.unshift(path) unless $;.include?(path)
+	require require_file	
 end
 
 require 'src/database'
