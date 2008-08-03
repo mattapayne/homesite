b2d528ce3becad3a609095dc04ca0a285a2bf295
matt <paynmatt@gmail.com>
2008-08-03 18:59:03 -0400

    Fixing load path issues

--- application.rb
+++ application.rb
@@ -1,17 +1,25 @@
 require 'rubygems'
 
+module VendorLoader
+	
+	def self.load
+		local_dir = File.expand_path(File.join(File.dirname(__FILE__), "vendor"))
+		MattPayne::Config.vendored_items.each do |vendor_dir, require_file|
+			path = File.expand_path(File.join(local_dir, vendor_dir.to_s))
+			$:.unshift(path) unless $:.include?(path)
+			puts "Added #{path} to the load path"
+			require require_file
+		end	
+	end
+	
+end
+
 APP_ENV = "development" unless defined?(APP_ENV)
 
 require 'src/core_extensions'
 require 'src/config'
 
-local_dir = File.expand_path(File.dirname(__FILE__))
-
-MattPayne::Config.vendored_items.each do |vendor_dir, require_file|
-	path = File.expand_path(File.join(local_dir, vendor_dir.to_s))
-	$:.unshift(path) unless $:.include?(path)
-	require require_file	
-end
+VendorLoader.load
 
 require 'src/database'
 require 'src/models'
