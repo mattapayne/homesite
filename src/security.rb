a9836b702b60dcbc985026e1e74f4c70c40a47ef
matt <paynmatt@gmail.com>
2008-08-02 22:25:47 -0400

    Further work

---
+++ src/security.rb
@@ -0,0 +1,24 @@
+module MattPayne
+	
+	module Security
+		
+		def login(username, password)
+			if ((username == MattPayne::Config.admin_username) && 
+				(password == MattPayne::Config.admin_password))
+					session[:logged_in] = true
+					return true
+			end
+			return false
+		end
+		
+		def logout
+			session.delete(:logged_in)
+		end
+		
+		def logged_in?
+			!session[:logged_in].nil? && session[:logged_in] == true
+		end
+		
+	end
+	
+end
