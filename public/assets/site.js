1abb25384a28b0117d9f691184af55c5b112a064
matt <paynmatt@gmail.com>
2008-08-30 16:06:14 -0400

    Adding support for stupid ie

--- public/assets/site.js
+++ public/assets/site.js
@@ -1,6 +1,19 @@
 function setCursor()
 {
+    if(document.body && document.body.getElements)
+    {
+        alert("Browser supports getting elements by class");
+    }
+    else
+    {
+        alert("Browser does not support gettin elements by class");
+    }
     elem = document.body.getElements("*[class=select]");
+    if(elem)
+    {
+        alert("Got elem");
+    }
+    
     if(elem && elem.length && elem[0] && elem[0].focus)
     {
         elem[0].focus();
