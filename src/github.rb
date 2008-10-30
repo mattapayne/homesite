83a3f6ec24c453a10e46d49d8261b8da6de5e4db
Matt Payne <matt@matt-laptop.(none)>
2008-10-30 00:38:20 -0400

    Altered more look and feel. Also wrapped calls to GitHub and Tumblr in timeouts to avoid the page taking forever to load when one of those services is slow

--- src/github.rb
+++ src/github.rb
@@ -1,6 +1,7 @@
 require 'open-uri'
 require 'xmlsimple'
 require 'ostruct'
+require 'timeout'
 
 module MattPayne
 
@@ -9,14 +10,18 @@ module MattPayne
     def self.repositories
       repos = []
       begin
-        xml = open(MattPayne::Config.github_url).read
-        tmp = xml.blank? ? {} : XmlSimple.xml_in(xml)
-        tmp = extract_repositories(tmp)
-        repos = tmp.inject([]) do |arr, r|
-          h = {:name => r["name"].first, :url => r["url"].first}
-          arr << OpenStruct.new(h) 
-          arr
+        Timeout::timeout(10) do
+          xml = open(MattPayne::Config.github_url).read
+          tmp = xml.blank? ? {} : XmlSimple.xml_in(xml)
+          tmp = extract_repositories(tmp)
+          repos = tmp.inject([]) do |arr, r|
+            h = {:name => r["name"].first, :url => r["url"].first}
+            arr << OpenStruct.new(h) 
+            arr
+          end
         end
+      rescue Timeout::Error => e
+        #swallow
       rescue
         #swallow
       end
--- src/tumblr.rb
+++ src/tumblr.rb
@@ -1,6 +1,7 @@
 require 'open-uri'
 require 'xmlsimple'
 require 'ostruct'
+require 'timeout'
 
 module MattPayne
 
@@ -9,14 +10,18 @@ module MattPayne
     def self.posts
       posts = []
       begin
-        xml = open(MattPayne::Config.tumblr_url).read
-        tmp = xml.blank? ? {} : XmlSimple.xml_in(xml)
-        tmp = extract_posts(tmp)
-        posts = tmp.inject([]) do |arr, p|
-          h = {:url => p["url"], :title => p["regular-title"].first, :date => p["date"]}
-          arr << OpenStruct.new(h)
-          arr
-        end 
+        Timeout::timeout(5) do
+          xml = open(MattPayne::Config.tumblr_url).read
+          tmp = xml.blank? ? {} : XmlSimple.xml_in(xml)
+          tmp = extract_posts(tmp)
+          posts = tmp.inject([]) do |arr, p|
+            h = {:url => p["url"], :title => p["regular-title"].first, :date => p["date"]}
+            arr << OpenStruct.new(h)
+            arr
+          end 
+        end
+      rescue Timeout::Error => e
+        #swallow
       rescue
         #swallow
       end
